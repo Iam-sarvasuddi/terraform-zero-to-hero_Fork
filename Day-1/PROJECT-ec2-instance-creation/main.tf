0ecbc4725f661d501eb0613ea5be11f282b142be
Saikumar Sarvasuddi <saikumarsarvasuddi2023@gmail.com>
2024-05-23 16:20:20 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,8 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_instance" "Saikumar_test" {
+    ami           = "ami-0069eac59d05ae12b"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
+    subnet_id = "	subnet-093899861eddc24e6"
 }
