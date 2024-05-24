7aa6200284f66e8e5727e0e84c5467422027f1e1
Saikumar Sarvasuddi <saikumarsarvasuddi2023@gmail.com>
2024-05-24 19:34:06 +0530

    Update main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -11,14 +11,3 @@ resource "aws_instance" "abhishek" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "abhishek-s3-demo-xyz" # change this
 }
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
