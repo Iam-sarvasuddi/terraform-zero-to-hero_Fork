cfef4719d5497b6edcdcf163794a599a3ac3b640
Saikumar Sarvasuddi <saikumarsarvasuddi2023@gmail.com>
2024-05-24 19:34:24 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -3,7 +3,4 @@ terraform {
     bucket         = "abhishek-s3-demo-xyz" # change this
     key            = "abhi/terraform.tfstate"
     region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
 }
